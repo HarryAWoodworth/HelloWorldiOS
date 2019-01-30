c8f2621eba42514afbf37875b4269f81ce1abfb8
Harry Woodworth <hwoodwo1@bl-n26.pods.bu.int>
2019-01-30 13:12:03 -0500

    Add text to speech action

--- HelloWorldInIOS/HelloWorldInIOS/ViewController.swift
+++ HelloWorldInIOS/HelloWorldInIOS/ViewController.swift
@@ -8,11 +8,21 @@
 //  References:
 //      https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/ConnectTheUIToCode.html#//apple_ref/doc/uid/TP40015214-CH22-SW1
 //      https://stackoverflow.com/questions/22561926/ios-text-to-speech-api
+//      https://stackoverflow.com/questions/26195262/how-to-create-a-global-variable
+//      https://developer.apple.com/documentation/avfoundation/avspeechutterance/1619708-rate
+//
 //
 
 import UIKit
+import AVFoundation // Imcluded for AVSpeechSynthesizer
 
 class ViewController: UIViewController {
+    
+    //MARK: Struct
+    struct SpeechSynthesizer
+    {
+        static var synthesizer = AVSpeechSynthesizer()
+    }
     
     //MARK: Properties
     @IBOutlet weak var speakTextField: UITextField!
@@ -20,14 +30,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    // Activate text to speech with the text in speakTextField
+    @IBAction func speakUtterance(_ sender: UIButton)
+    {
+        // Create the utterance using speakTextField
+        let utterance = AVSpeechUtterance(string : speakTextField.text!)
+        
+        // Set the rate at which the text is spoken
+        utterance.rate = 0.3
+        
+        // Speak
+        SpeechSynthesizer.synthesizer.speak(utterance)
+    }
+    
 }
 

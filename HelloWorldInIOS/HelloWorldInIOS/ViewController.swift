4d3e3e463886966b55585816569018d5aed5535d
Harry Woodworth <hwoodwo1@bl-n26.pods.bu.int>
2019-01-30 12:42:54 -0500

    Add UI to Main storyboard

--- HelloWorldInIOS/HelloWorldInIOS/ViewController.swift
+++ HelloWorldInIOS/HelloWorldInIOS/ViewController.swift
@@ -5,11 +5,19 @@
 //  Created by Harry Woodworth on 1/28/19.
 //  Copyright © 2019 Harry Woodworth. All rights reserved.
 //
+//  References:
+//      https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/ConnectTheUIToCode.html#//apple_ref/doc/uid/TP40015214-CH22-SW1
+//      https://stackoverflow.com/questions/22561926/ios-text-to-speech-api
+//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var speakTextField: UITextField!
+    @IBOutlet weak var speakButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
